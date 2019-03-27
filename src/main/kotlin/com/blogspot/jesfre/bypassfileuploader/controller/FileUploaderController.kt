e2a2dc0b9e43c99a1870ad81cefd58c3fc6358fe
Jorge Ruiz-Aquino <jesfre.gy@gmail.com>
2019-03-27 05:25:07 -0400

    Added support for uploading multiple files. Closes #5

--- src/main/kotlin/com/blogspot/jesfre/bypassfileuploader/controller/FileUploaderController.kt
+++ src/main/kotlin/com/blogspot/jesfre/bypassfileuploader/controller/FileUploaderController.kt
@@ -1,5 +1,6 @@
 package com.blogspot.jesfre.bypassfileuploader.controller
 
+import com.blogspot.jesfre.bypassfileuploader.service.FileUploadService
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
@@ -7,18 +8,25 @@ import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.multipart.MultipartFile
 
-@Controller
+@Controller("/")
 class FileUploaderController {
 
-    @GetMapping("uploadfile")
+    val fileUploadService: FileUploadService
+
+    constructor(fileUploadService: FileUploadService) {
+        this.fileUploadService = fileUploadService
+    }
+
+    @GetMapping
     fun showUploadForm(model: Model): String {
         return "fileuploadform"
     }
 
-    @PostMapping("uploadfile")
-    fun handleImagePost(@RequestParam("file") file: MultipartFile): String {
+    @PostMapping
+    fun handleImagePost(@RequestParam("file") fileArray: Array<MultipartFile>): String {
         println("Entering handleImagePost method...")
-        // TODO call to file service
+        for(file in fileArray)
+            fileUploadService.saveFile(file)
 
         println("Returning to uploadfile...")
         return "redirect:/uploadfile"
