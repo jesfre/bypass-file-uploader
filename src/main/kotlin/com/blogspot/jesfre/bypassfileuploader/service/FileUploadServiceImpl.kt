3738d7fc90bdc2a6f5fb790a07f7a367ff798180
Jorge Ruiz-Aquino <jesfre.gy@gmail.com>
2019-04-16 00:21:01 -0400

    Added configuration for output directory. Incremented max file size to 800MB. Closes #4

--- src/main/kotlin/com/blogspot/jesfre/bypassfileuploader/service/FileUploadServiceImpl.kt
+++ src/main/kotlin/com/blogspot/jesfre/bypassfileuploader/service/FileUploadServiceImpl.kt
@@ -1,11 +1,13 @@
 package com.blogspot.jesfre.bypassfileuploader.service
 
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
 import org.springframework.web.multipart.MultipartFile
 import java.io.File
 
 import java.io.IOException
+import java.lang.IllegalArgumentException
 import java.nio.file.Files
 import java.nio.file.Path
 import java.nio.file.Paths
@@ -15,11 +17,22 @@ import java.nio.file.StandardOpenOption
 @Service
 class FileUploadServiceImpl : FileUploadService {
 
+    @Value("\${output.directory}")
+    private lateinit var outputPathString:String
+
     override fun saveFile(file: MultipartFile, pathString:String) {
-        var pathToSave = Paths.get("D:/dev/bypass-file-uploader/outputs/")
+        println("Files will be stored to base directory $outputPathString")
+
+        if(pathString.contains("../")) {
+            throw IllegalArgumentException("Invalid path. Use normalized paths only.")
+        }
+
+        var pathToSave = Paths.get(outputPathString)
         try{
             if(pathString != "") {
                 var path:Path = Paths.get(pathString)
+                println("Input sub-oath $path")
+
                 if(null != path.root) {
                    path =  path.subpath(0   , path.nameCount)
                 }
